98cea1b558c8a4186a5d7480edbbb94f48fe6171
«Yuliya <YBelik@renins.com>
2023-09-16 21:46:21 +0200

    Logs removed

--- app/src/main/java/com/example/hackzurich/domain/model/UserMessage.kt
+++ app/src/main/java/com/example/hackzurich/domain/model/UserMessage.kt
@@ -1,7 +1,6 @@
 package com.example.hackzurich.domain.model
 
 import android.graphics.Bitmap
-import android.util.Log
 import com.example.hackzurich.domain.BaseModelPayload
 
 data class UserMessage(
@@ -9,15 +8,12 @@ data class UserMessage(
     val image: String? = null,
     val bitmap: Bitmap? = null
 ): BaseModel {
-    val TAG = this.javaClass.simpleName
 
     override fun isIdDiff(other: BaseModel): Boolean {
-        Log.d(TAG, "isIdDiff: ")
         return other is UserMessage
     }
 
     override fun isContentDiff(other: BaseModel): Boolean {
-        Log.d(TAG, "isContentDiff: ")
         if (other !is UserMessage) return false
         if (other.message != this.message) return false
         if (other.image != this.image) return false
@@ -25,7 +21,6 @@ data class UserMessage(
     }
 
     override fun getPayloadDiff(other: BaseModel): MutableList<Any> {
-        Log.d(TAG, "getPayloadDiff: ")
         val payloads = mutableListOf<Any>()
         if (other !is UserMessage)
             return payloads
